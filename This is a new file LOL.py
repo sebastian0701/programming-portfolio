47823f424996c68da89c82684963e226aadf713b
Sebastian Cisneros <sebastiancisneros@Alveyworld-23.local>
2015-09-14 09:51:04 -0600

    Band Name

---
+++ This is a new file LOL.py
@@ -0,0 +1,11 @@
+amount = float(raw_input("Enter amount: "))
+inrate = float (raw_input("Enter Interest rate: "))
+period = int(raw_input)("Enter period: "))
+value = 0
+year = 1 
+while year <= period:
+		value <= period:
+				value = amount + (inrate * amount)
+				print "year %d Rs. %.2f" % (year , value)
+				amount = value
+				year = year + 1
