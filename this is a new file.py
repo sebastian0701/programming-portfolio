4c42f907abc8494935e109c8c434cc2a977fc9d7
Sebastian Cisneros <sebastiancisneros@Alveyworld-23.local>
2015-09-14 10:18:55 -0600

    Learning about statements

---
+++ this is a new file.py
@@ -0,0 +1,16 @@
+# Learning about is statements
+
+number = int(raw_input("Enter your age"))
+
+if number < 5:
+	print "You are not in school yet"
+elif number < 11:
+	print "You are in elementary school"
+elif number < 15:
+	print "you are still in middle school/intermediate school"
+elif number < 19:
+	print "You are in high school"
+else:
+	print "You know everything"
+	
+	
