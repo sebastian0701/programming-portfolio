e25c283bb958a4fc4f88ac6c4a5ade25a9059020
Sebastian Cisneros <sebastiancisneros@Alveyworld-23.local>
2016-01-06 10:28:19 -0700

    I did it

--- madlib.py
+++ madlib.py
@@ -1,5 +1,17 @@
 #mad lib program
 
 adj1 = raw_input("Enter an adjective: ")
+adj2 = raw_input("Enter an adjective: ")
+adj3 = raw_input("Enter a Bird type: ")
+adj4 = raw_input("Room in a house: ")
+verb1 = raw_input("Enter a verb: ")
+verb2 = raw_input("Enter a verb: ")
+name1 = raw_input("Enter a name: ")
+noun1 = raw_input("Enter a noun: ")
+
+
 
 print "It was a " + adj1 + ", cold November day."
+print " I woke up to the " + adj2 + ", smell of " + adj3 + ", roasting in the " + adj4 + ", downstairs."
+print " I " + verb1 + ", down the stairs to see if I could help " + verb2 + ", the dinner."
+print " My mom said, see if " + name1 + ", needs a fresh " + noun1 + "."
