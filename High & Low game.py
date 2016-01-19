fee26160c7a75d4c6bde3a727742e0da38ed8cbf
Sebastian Cisneros <sebastiancisneros@Alveyworld-23.local>
2016-01-19 10:22:36 -0700

    I added 2 games

---
+++ High & Low game.py
@@ -0,0 +1,27 @@
+#High & Low game
+
+import random 
+
+attempts = 1
+secret_number = random.randint(1,1000)
+isCorrect = False
+guess = int(input("Take a guess: "))
+
+while secret_number != guess and attempts < 10:
+
+    if guess < secret_number:
+        print("Higher...")
+    elif guess > secret_number:
+        print("Lower...")
+    guess = int(input("Take a guess: "))
+    attempts += 1
+
+if attempts == 6:
+    print("\nSorry you reached the maximum number of tries")
+    print("The secret number was ",secret_number) 
+
+else:
+    print("\nYou guessed it! The number was " ,secret_number)
+    print("You guessed it in ", attempts,"attempts")
+
+input("\n\n Press the enter key to exit")           
